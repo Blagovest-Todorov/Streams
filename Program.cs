bdac86b1bd4444db890dbe8644e71d63d92c52eb
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-06-01 14:29:19 +0300

    add Program.cs, add input.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt") )
+            {
+                string line = await sr.ReadLineAsync();
+
+                using (StreamWriter wrt = new StreamWriter("output.txt")) 
+                {
+                    int count = 1;
+
+                    while (line != null)
+                    {
+                        await wrt.WriteAsync($"{count}. {line}");
+                        count++;
+                        line = await sr.ReadLineAsync();
+                    }
+                }  
+            }
+        }
+    }
+}
